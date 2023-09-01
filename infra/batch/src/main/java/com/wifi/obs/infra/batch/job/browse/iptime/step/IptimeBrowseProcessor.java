1555a0656b4ef01f8a4b5013f01ea0a99a05cfc9
belljun3395 <195850@jnu.ac.kr>
2023-09-01 18:01:02 +0900

    OBS-*** refactor: 구현 부분 BLANK 처리

--- infra/batch/src/main/java/com/wifi/obs/infra/batch/job/browse/iptime/step/IptimeBrowseProcessor.java
+++ infra/batch/src/main/java/com/wifi/obs/infra/batch/job/browse/iptime/step/IptimeBrowseProcessor.java
@@ -1,11 +1,10 @@
 package com.wifi.obs.infra.batch.job.browse.iptime.step;
 
-import com.wifi.observer.client.wifi.client.iptime.IptimeBrowseClientImpl;
-import com.wifi.observer.client.wifi.dto.request.iptime.IptimeBrowseRequest;
 import com.wifi.observer.client.wifi.dto.response.AuthInfo;
 import com.wifi.observer.client.wifi.dto.response.iptime.IptimeOnConnectUserInfosResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
@@ -15,21 +14,8 @@ import org.springframework.stereotype.Component;
 public class IptimeBrowseProcessor
 		implements ItemProcessor<AuthInfo, IptimeOnConnectUserInfosResponse> {
 
-	private final IptimeBrowseClientImpl iptimeBrowseClient;
-
 	@Override
 	public IptimeOnConnectUserInfosResponse process(AuthInfo item) {
-		IptimeBrowseRequest request =
-				IptimeBrowseRequest.builder().authInfo(item.getInfo()).host(item.getHost()).build();
-		log.debug(">>> request : {}", request);
-
-		IptimeOnConnectUserInfosResponse browseInfo = iptimeBrowseClient.query(request);
-
-		if (browseInfo.getResponse().isEmpty()) {
-			log.error("browseInfo is empty host : {}", browseInfo.getHost());
-			return null;
-		}
-
-		return browseInfo;
+		throw new NotImplementedException("구현해주세요");
 	}
 }
